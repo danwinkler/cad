33d0a091442e465b83c4555512f161b1d46b2e66
Daniel Winkler <dan@danwink.com>
2023-08-17 19:12:29 -0500

    added grid of images of projects

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Various CAD projects, mostly for 3D printing, mostly using OpenSCAD.
 
 Projects are from 2014 to present.
+
+![Grid of 3D printed objects](https://raw.githubusercontent.com/danwinkler/cad/main/src/cad/utils/screenshotter/grid.png)
---
+++ src/cad/utils/screenshotter/screenshotter.py
@@ -0,0 +1,75 @@
+import subprocess
+from pathlib import Path
+
+from PIL import Image
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+def get_projects():
+    projects = []
+    for year in (BASE_DIR / "projects").iterdir():
+        if year.is_dir():
+            for project in year.iterdir():
+                if project.is_dir() and project.name != "__pycache__":
+                    projects.append(project)
+    return projects
+
+
+def get_scad_files(path):
+    return list(path.rglob("*.scad"))
+
+
+def capture_screenshot(scad_file, image_path):
+    pgm = "C:\Program Files\OpenSCAD\openscad.exe"
+    subprocess.call([pgm, "-o", image_path, scad_file])
+
+
+def main():
+    # Generate screenshots for all projects
+    projects = get_projects()
+    for project in projects:
+        scad_files = get_scad_files(project)
+        if scad_files:
+            year = project.parent.name
+            project_name = project.name
+
+            image_path = (
+                BASE_DIR
+                / "utils"
+                / "screenshotter"
+                / "images"
+                / f"{year}_{project_name}.png"
+            )
+
+            if not image_path.exists():
+                print(f"Capturing screenshot for {project_name}")
+                capture_screenshot(
+                    scad_files[0],
+                    image_path,
+                )
+
+    # Generate grid of screenshots
+    images = list((BASE_DIR / "utils" / "screenshotter" / "images").rglob("*.png"))
+    image_size = 150
+    grid_width = 5
+    grid_height = len(images) // grid_width
+
+    grid = Image.new(
+        "RGB",
+        (grid_width * image_size, grid_height * image_size),
+        color=(255, 255, 255),
+    )
+
+    for i, image in enumerate(images):
+        x = (i % grid_width) * image_size
+        y = (i // grid_width) * image_size
+        to_paste = Image.open(image)
+        to_paste.thumbnail((image_size, image_size))
+        grid.paste(to_paste, (x, y))
+
+    grid.save(BASE_DIR / "utils" / "screenshotter" / "grid.png")
+
+
+if __name__ == "__main__":
+    main()
